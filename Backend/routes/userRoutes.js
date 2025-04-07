09f228dfffa851cc2969eb5a6c7c3301244cb8c9
Paras <mehtaparas1901@gmail.com>
2025-04-07 12:43:57 +0530

    implemented signin route and validation

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const zod = require("zod");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-const {UserModal} = require('../db/db');
+const {UserModal, AccountModal} = require('../db/db');
 
 const router = Router();
 
@@ -15,7 +15,10 @@ const signupValidation = zod.object({
   password: zod.string().trim().min(6),
 });
 
-
+const signinValidation = zod.object({
+    username: zod.string().trim().email(),
+    password: zod.string().trim().min(6),
+});
 
 
 //[auth routes]
@@ -73,4 +76,47 @@ router.post("/signup", async (req, res) => {
 });
 
 
+//route = /api/v1/user/signin
+router.post("/signin", async (req, res) => {
+    const createPayload = req.body;
+    const parsePayload = signinValidation.safeParse(createPayload);
+  
+    //check shcema validation , if zod validation failed (i.e user provide false inputs)
+    if (!parsePayload.success) {
+      return res.status(400).json({
+        msg: "Incorrect inputs",
+      });
+    }
+  
+    //check if email is correct
+    const user = await UserModal.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json({
+        msg: "Invalid Email",
+      });
+    }
+  
+    //check correct password provided or not
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        msg: "Invalid Password",
+      });
+    }
+  
+    try {
+      //if user present in db then Generate JWT token and send token to user
+      const userId = user._id;
+      const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+      return res.status(200).json({
+        msg: `Welcome ${user.firstName}`,
+        token: token,
+      });
+    } catch (error) {
+      return res.json({
+        msg: "error while logginIN",
+      });
+    }
+});
+  
   
