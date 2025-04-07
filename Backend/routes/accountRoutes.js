67b73911c8bf312761f0efa532fdc8bad6774104
Paras <mehtaparas1901@gmail.com>
2025-04-07 20:04:35 +0530

    implemented balance route include mongodb transaction

--- Backend/routes/accountRoutes.js
+++ Backend/routes/accountRoutes.js
@@ -13,7 +13,7 @@ router.get("/receiverdetails/:userId", userMiddleware, async (req, res) => {
     try {
       //find if receiver exist in our db if exist than send his/her name as response
       const receiver = await UserModal.findById(userId);
-      console.log(receiver);
+      //console.log(receiver);
   
       const name = receiver.firstName;
       return res.status(200).json({
@@ -48,6 +48,69 @@ router.get("/balance", userMiddleware, async (req, res) => {
 });
 
 
+
+//An endpoint for user to transfer money to another account. route = /api/v1/account/transfer
+router.patch("/transfer", userMiddleware, async (req, res) => {
+    const senderId = req.userId;
+    const { recieverId, amount } = req.body; 
+  
+    if (!recieverId || !amount) {
+      return res.json({ msg: "Missing required fields" });
+    }
+  
+    try {
+      const session = await mongoose.startSession(); //start session
+      session.startTransaction();                    //start the transaction
+  
+      const senderAccount = await AccountModal.findOne({ userId: senderId }).session(session);
+      const reciverAccount = await AccountModal.findOne({ userId: recieverId }).session(session);
+  
+  
+      //if senderr not found or balance of sender is less than amount to be transferred
+      if (!senderAccount || senderAccount.balance < amount) {
+        await session.abortTransaction()  //abort transaction
+        session.endSession();
+        return res.status(400).json({
+          msg: "Insufficient Account Balance",
+        });
+      }
+  
+      //if recevier not found
+      if (!reciverAccount) {
+        await session.abortTransaction()  //abort transaction
+        session.endSession();
+        return res.status(400).json({
+          msg: "Receiver account not found",
+        });
+      }
+      
+      //Decrement the amount of sender
+      await AccountModal.updateOne(
+        { userId: senderId },
+        { $inc: { balance: -amount } }
+      ).session(session);
+  
+      //Increment the amount of receiver
+      await AccountModal.updateOne(
+        { userId: recieverId },
+        { $inc: { balance: amount } }
+      ).session(session);
+  
+      //Commit the transaction
+      await session.commitTransaction();
+      session.endSession()
+      res.status(200).json({
+        msg: "Transaction successful",
+      });
+    } 
+    catch (error) {
+      await session.abortTransaction()
+      session.endSession()
+      return res.status(500).json({
+        msg : "Transcation failed due to an error", error
+      })
+    }
+});
 
 
 module.exports = router;
