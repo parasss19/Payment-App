af32997406c5c991fe05432c7d882cdb7bc588b9
Paras <mehtaparas1901@gmail.com>
2025-04-11 15:56:53 +0530

    ui: Dashboard page added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,20 @@
+import AllUsers from '@/components/AllUsers'
+import BalanceCard from '@/components/BalanceCard'
+import Filter from '@/components/Filter'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      {/* Balance Card */}
+      <BalanceCard/>
+
+      {/* Filter */}
+      <Filter/>
+
+      {/* All users */}
+      <AllUsers/>
+    </div>
   )
 }
 
