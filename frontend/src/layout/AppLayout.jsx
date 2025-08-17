9daf4eccc593744b3b5c8bc013a7ec4eec666b58
Paras <mehtaparas1901@gmail.com>
2025-08-17 20:29:30 +0530

    gradient applied

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const AppLayout = () => {
   return (
-    <div className='min-h-screen'>
-        <div className='px-6 py-4'>
-            <Header/>
-            <main>
-                <Outlet/>
-            </main>
-            <Footer/>
+    <div className="min-h-screen w-full bg-white relative">
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `radial-gradient(125% 125% at 50% 90%, #ffffff 40%, #14b8a6 100%)`,
+          backgroundSize: "100% 100%",
+        }}
+      />
+      {/* Layout */}
+      <div className="relative ">
+        <div className="px-6 py-4">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
         </div>
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
