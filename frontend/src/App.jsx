a93181d5b0327e6c87ab037009c0e055cbb67e44
Paras <mehtaparas1901@gmail.com>
2025-08-30 16:27:00 +0530

    implemented new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,19 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css'
 
 import AppLayout from './layout/AppLayout';
-
 import HomePage from './pages/HomePage';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
 import Dashboard from './pages/Dashboard';
 import SendMoney from './pages/SendMoney';
-import { ToastContainer } from "react-toastify";
+import { Toaster } from 'react-hot-toast';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Terms from './pages/Terms';
+import PageNotFound from './pages/PageNotFound';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
+import AuthPage from './pages/AuthPage';
+import P2PTransfer from './pages/P2PTransfer';
+import AddFunds from './pages/AddFunds';
+import Transactions from './pages/Transactions';
 
 
 function App() {
@@ -18,33 +24,93 @@ function App() {
       element: <AppLayout />,
 
       children: [
+        //Public Routes
         {
           path: '/',
-          element: <HomePage />
+          element:(
+            <PublicRoute>
+              <HomePage />
+            </PublicRoute>
+          )
         }, 
         {
-          path: '/signup',
-          element: <Signup/>
+          path: '/auth',
+          element:(
+            <PublicRoute>
+              <AuthPage/>
+            </PublicRoute>
+          ) 
         },
-        {
-          path: '/signin',
-          element: <Signin/>
-        },
+
+        //Protected Routes
         {
           path: '/dashboard',
-          element: <Dashboard/>
+          element: (
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          )
+        },
+        {
+          path: '/addFunds',
+          element: (
+            <ProtectedRoute>
+              <AddFunds />
+            </ProtectedRoute>
+          )
+        },
+        {
+          path: '/transactions',
+          element: (
+            <ProtectedRoute>
+              <Transactions />
+            </ProtectedRoute>
+          )
+        },
+        {
+          path: '/p2ptransfer',
+          element: (
+            <ProtectedRoute>
+              <P2PTransfer />
+            </ProtectedRoute>
+          )
         },
         {
           path: '/send/:recieverId',
-          element: <SendMoney/>
-        }
+          element: (
+            <ProtectedRoute>
+              <SendMoney />
+            </ProtectedRoute>
+          )
+        },
+
+        //Terms and policy routes
+        {
+          path: '/privacy',
+          element: (
+            <PrivacyPolicy/>
+          )
+        },
+        {
+          path: '/terms',
+          element: (
+            <Terms/>
+          )
+        },
+
+        //Catch-all route for 404 
+        {
+          path: '*',
+          element: <PageNotFound/>,
+        },
+        
       ],
     },
   ]);
 
   return (
     <>
-      <ToastContainer position="top-right" autoClose={3000} />
+      <Toaster position="top-center" />
       <RouterProvider router={router}/>
     </>
   )
