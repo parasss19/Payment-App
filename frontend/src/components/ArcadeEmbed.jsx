ec7de571cbb7dec141b0d811b1ecb66522920638
Paras <mehtaparas1901@gmail.com>
2025-08-30 18:23:31 +0530

    added demo video

---
+++ frontend/src/components/ArcadeEmbed.jsx
@@ -0,0 +1,15 @@
+export function ArcadeEmbed() {
+  return (
+    <div style={{ position: 'relative', paddingBottom: 'calc(49.333333333333336% + 41px)', height: 0, width: '100%' }}>
+      <iframe
+        src="https://demo.arcade.software/o1l7Y8XI6N2tkAeZahHm?embed&embed_mobile=inline&embed_desktop=inline&show_copy_link=true"
+        title="PayEasy"
+        frameBorder="0"
+        loading="lazy"
+        allowFullScreen
+        allow="clipboard-write"
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', colorScheme: 'light' }}
+      />
+    </div>
+  )
+}
