9422001af67157a94d63cdf03fc15fcbaac475f6
Paras <mehtaparas1901@gmail.com>
2025-08-30 15:52:13 +0530

    updated server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,35 @@
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/connectDB.js';
+import cookieParser from "cookie-parser";
 
-const app = require('./index');
+import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import accountRouter from './routes/accountRoutes.js';
+// import paymentsRouter from './routes/paymentsRoutes.js';
 
-const port = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectDB();
 
-app.listen(port, ()=>{
-    console.log(`The app is running on port ${port}`);
+//CORS and cookies
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials:true,
+}));
+app.use(cookieParser());
+
+//JSON body parser
+app.use(express.json());
+
+//handling routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/account', accountRouter);
+// app.use('/api/v1/payments', paymentsRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`);
 })
