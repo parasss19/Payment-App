d40f1a935a90bd9527a15a2fb5e0a062753c476e
Paras <mehtaparas1901@gmail.com>
2025-09-03 16:49:20 +0530

    remove redundant code

--- Backend/server.js
+++ Backend/server.js
@@ -7,17 +7,18 @@ import cookieParser from "cookie-parser";
 import authRouter from './routes/authRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import accountRouter from './routes/accountRoutes.js';
-// import paymentsRouter from './routes/paymentsRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
-//CORS and cookies
+//CORS setup
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials:true,
 }));
+
+
 app.use(cookieParser());
 
 //JSON body parser
@@ -27,7 +28,7 @@ app.use(express.json());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/account', accountRouter);
-// app.use('/api/v1/payments', paymentsRouter);
+
 
 
 app.listen(PORT, () => {
